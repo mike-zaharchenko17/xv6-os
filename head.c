4b0cef39fe55ea0b4f7b3c747d0fc3d569545471
mike-zaharchenko17 <129902761+mike-zaharchenko17@users.noreply.github.com>
2025-12-06 03:29:54 +0000

    Removed from syscall.c; didn't need it there. Wrote parser logic

--- head.c
+++ head.c
@@ -1,3 +1,21 @@
+#include "types.h"
+#include "stat.h"
+#include "user.h"
+
+int all_digits(char *s) {
+    if (*s == 0) {
+        return 0;
+    }
+
+    for (; *s; s++) {
+        if (*s < '0' || *s > '9') {
+            return 0;
+        }
+    }
+
+    return 1;
+}
+
 int main(int argc, char *argv[]) {
     /*
     head FILE
@@ -36,4 +54,43 @@ int main(int argc, char *argv[]) {
         case 2.3: multiple files
             read 10 (default) lines from each of those files
     */
+
+    int N = 10; //default
+    int i = 1;
+
+    // if arguments are provided and there is a flag
+    if (i < argc && argv[i][0] == '-') {
+        // move arg pointer to one after -
+        char *arg = argv[i] + 1;
+
+        // handle -n case
+        if (arg[0] == 'n') {
+            printf(1, "-n flag detected\n");
+            // handle -nN case
+            if (all_digits(arg + 1)) {
+                printf(1, "handling -nN case\n");
+                N = atoi(arg + 1);
+                printf(1, "head: N = %d\n", N);
+                i++;
+            // handle -n N case
+            } else if (i + 1 < argc && all_digits(argv[i+1])) {
+                printf(1, "handling -n N case\n");
+                N = atoi(argv[i+1]);
+                printf(1, "head: N = %d\n", N);
+                i += 2;
+            // fallthrough
+            } else {
+                printf(1, "head: invalid -n usage\n");
+                exit();
+            }
+        // handle -N case
+        } else if (all_digits(arg)) {
+            printf(1, "handling -N case\n");
+            N = atoi(arg);
+            printf(1, "head: N = %d\n", N);
+            i++;
+        }
+    }
+
+    exit();
 }
