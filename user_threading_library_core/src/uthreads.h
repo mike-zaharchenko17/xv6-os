93d8c84324c14b260ec0564ed7ef44d680955c71
mike-zaharchenko17 <129902761+mike-zaharchenko17@users.noreply.github.com>
2025-12-11 18:48:49 +0000

    More thread layout setup

--- user_threading_library_core/src/uthreads.h
+++ user_threading_library_core/src/uthreads.h
@@ -3,4 +3,23 @@
 
 enum threadstate { T_UNUSED, T_RUNNABLE, T_RUNNING, T_SLEEPING, T_ZOMBIE };
 
+// thread structure
+struct thread {
+    int tid;
+    enum threadstate tstate;
+
+    char *stack; // base of stack
+    uint sp; // stack ptr
+};
+
+// thread table
+struct thread threads[MAX_THREADS];
+
+// current thread ptr
+struct thread *current_thread;
+
+// for allocating TIDs
+int next_tid = 1;
+
+
 
