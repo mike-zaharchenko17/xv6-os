a6345f4404625e337ba16ce1683779a0a7ca078d
mike-zaharchenko17 <129902761+mike-zaharchenko17@users.noreply.github.com>
2025-12-12 17:02:51 +0000

    Fixed up thread_init

--- user_threading_library_core/src/uthreads.c
+++ user_threading_library_core/src/uthreads.c
@@ -9,16 +9,23 @@ int next_tid = 1;
 
 void thread_init(void) {
     for (int i = 0; i < MAX_THREADS; i++) {
+        threads[i].tid = -1;
         threads[i].tstate = T_UNUSED;
+
         threads[i].stack = 0;
         threads[i].sp = 0;
+
+        threads[i].start_routine = 0;
+        threads[i].arg = 0;
+        threads[i].retval = 0;
+
+        threads[i].joiner_tid = -1;
+        threads[i].qnext = 0;
     }
 
     current_thread = &threads[0];
     current_thread->tid = 0;
     current_thread->tstate = T_RUNNING;
-    current_thread->stack = 0;
-    current_thread->sp = 0;
 }
 
 int thread_self(void) {
