b16adf74528a54c539ae3dfe6da65005cbcb2591
mike-zaharchenko17 <129902761+mike-zaharchenko17@users.noreply.github.com>
2025-12-15 20:45:06 +0000

    cond_signal impl

--- user_threading_library_core/src/uthreads.c
+++ user_threading_library_core/src/uthreads.c
@@ -369,7 +369,13 @@ void cond_wait(cond_t *c, mutex_t *m) {
 }
 
 void cond_signal(cond_t *c) {
-    printf(1, "stub for cond_signal");
+    struct thread *waiter = wait_q_dequeue(&c->q);
+    if (!waiter) {
+        printf(1, "cond_signal: queue empty");
+        return;
+    }
+    waiter->tstate = T_RUNNABLE;
+
 }
 
 void cond_broadcast(cond_t *c) {
