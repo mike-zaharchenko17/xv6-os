1aac6bac2894ec3443f029dbd9c9a41c60be8e75
mike-zaharchenko17 <129902761+mike-zaharchenko17@users.noreply.github.com>
2025-12-18 01:32:21 +0000

    consumer impl

--- user_threading_library_core/examples/thread_safe_file_io.c
+++ user_threading_library_core/examples/thread_safe_file_io.c
@@ -58,16 +58,95 @@ static int read_n(int fd, const void *buf, int n) {
 }
 
 void consumer_process(void) {
+    // consumer does not write, so we close write end
+    close(fds[1]);
+
     // per process thread table init since threads are 1:N
     thread_init();
 
-    mutex_init(&read_lock);
-
     // same with synch primitive init
+    mutex_init(&read_lock);
+    mutex_init(&count_lock);
 
+    total_consumed = 0;
+
+    int tids[NCONS];
+
+    // create a thread for each of our producers
+    for (int i = 0; i < NCONS; i++) {
+        tids[i] = thread_create(consumer_thread, (void*)(i+1));
+        if (tids[i] < 0) {
+            printf(1, "consumer: thread_create failed\n");
+            exit();
+        }
+    }
+
+    for (int i = 0; i < NCONS; i++) {
+        thread_join(tids[i]);
+    }
+
+    printf(1, "All items processed. Consumers exiting. total=%d (expected %d)\n", total_consumed, NPROD * PER_PROD);
+
+    if (total_consumed != NPROD * PER_PROD) {
+        printf(1, "[FAIL] lost items!\n");
+        exit();
+    }
 }
 
+// void *producer_thread(void* arg) {
+//     int pid = (int) arg;
+
+//     for (int i = 0; i < PER_PROD; i++) {
+//         struct msg m;
+//         m.prod_id = pid;
+//         m.item = 1;
+
+//         mutex_lock(&write_lock);
+
+//         // write exactly one msg to the write end
+//         if (write_n(fds[1], &m, sizeof(m)) < 0) {
+//             printf(1, "producer %d: write failed\n", pid);
+//             mutex_unlock(&write_lock);
+//             exit();
+//         }
+
+//         printf(1, "Producer %d: produced item %d\n", pid, i);
+
+//         mutex_unlock(&write_lock);
+
+//         thread_yield();
+//     }
+
+//     return 0;
+// }
+
 void *consumer_thread(void *arg) {
+    int cid = (int) arg;
+
+    for (;;) {
+        struct msg m;
+
+        mutex_lock(&read_lock);
+        int r = readn(fds[0], &m, sizeof(m));
+        mutex_unlock(&read_lock);
+
+        if (r == 0) {
+            return 0;
+        }
+
+        if (r < 0) {
+            printf(1, "Consumer %d: read failed\n", cid);
+            exit();
+        }
+
+        printf(1, "Consumer %d: consumed item %d (from Producer %d)\n", cid, m.item, m.prod_id);
+
+        mutex_lock(&count_lock);
+        total_consumed++;
+        mutex_unlock(&count_lock);
+
+        thread_yield();
+    }
 
 }
 
